009bc6cb0ae21e7661c291b72004e0968c7b1880
Martin <martin.soethe@uni-flensburg.de>
2017-06-01 14:39:40 +0200

    Refactor appl.py

--- etrago/appl.py
+++ etrago/appl.py
@@ -12,108 +12,33 @@ __author__ = "tba"
 
 
 from egopowerflow.tools.tools import oedb_session
-from egopowerflow.tools.io import get_timerange, import_components, import_pq_sets,\
-    add_source_types, create_powerflow_problem
-from egopowerflow.tools.plot import add_coordinates, plot_line_loading,\
-     plot_stacked_gen
-from egoio.db_tables.model_draft import EgoGridPfHvBus as Bus, EgoGridPfHvLine as Line, EgoGridPfHvGenerator as Generator, EgoGridPfHvLoad as Load,\
-    EgoGridPfHvTransformer as Transformer, EgoGridPfHvTempResolution as TempResolution, EgoGridPfHvGeneratorPqSet as GeneratorPqSet,\
-    EgoGridPfHvLoadPqSet as LoadPqSet, EgoGridPfHvSource as Source #, EgoGridPfHvStorage,\
-#    EgoGridPfHvStoragePqSet
-
+from egopowerflow.tools.io import NetworkScenario
 import time
+from egopowerflow.tools.plot import plot_line_loading, plot_stacked_gen
 
 session = oedb_session()
+scenario = NetworkScenario(session, method='lopf', start_h=1, end_h=2,
+                           scn_name='Status Quo')
+network = scenario.build_network()
 
-scenario = 'Status Quo'
-
-# define relevant tables of generator table
-pq_set_cols_1 = ['p_set']
-pq_set_cols_2 = ['q_set']
-p_max_pu = ['p_max_pu']
-#storage_sets = ['inflow'] # or: p_set, q_set, p_min_pu, p_max_pu, soc_set, inflow
-
-# choose relevant parameters used in pf
-temp_id_set = 1
-start_h = 2286
-end_h = 2301
-
-# define investigated time range
-timerange = get_timerange(session, temp_id_set, TempResolution, start_h, end_h)
-
-# define relevant tables
-tables = [Bus, Line, Generator, Load, Transformer]
-
-# get components from database tables
-components = import_components(tables, session, scenario)
-
-# create PyPSA powerflow problem
-network, snapshots = create_powerflow_problem(timerange, components)
-
-# import pq-set tables to pypsa network (p_set for generators and loads)
-pq_object = [GeneratorPqSet, LoadPqSet]
-network = import_pq_sets(session=session,
-                         network=network,
-                         pq_tables=pq_object,
-                         timerange=timerange,
-                         scenario=scenario, 
-                         columns=pq_set_cols_1,                         
-                         start_h=start_h,
-                         end_h=end_h)
-
-# import pq-set table to pypsa network (q_set for loads)
-network = import_pq_sets(session=session,
-                         network=network,
-                         pq_tables=[LoadPqSet],
-                         timerange=timerange,
-                         scenario=scenario, 
-                         columns=pq_set_cols_2,                         
-                         start_h=start_h,
-                         end_h=end_h)
-                         
-network = import_pq_sets(session=session,
-                         network=network,
-                         pq_tables=[GeneratorPqSet],
-                         timerange=timerange,
-                         scenario=scenario, 
-                         columns=p_max_pu,                         
-                         start_h=start_h,
-                         end_h=end_h)   
-                      
-## import time data for storages:
-#network = import_pq_sets(session=session,
-#                         network=network,
-#                         pq_tables=[StoragePqSet],
-#                         timerange=timerange,
-#                         scenario=scenario, 
-#                         columns=storage_sets,                         
-#                         start_h=start_h,
-#                         end_h=end_h)          
-
-# set virtual storages to be extendable
+# data preparation
 network.storage_units.p_nom_extendable = True
-#network.storage_units.soc_cyclic = True        
-
-# add coordinates to network nodes and make ready for map plotting
-network = add_coordinates(network)
-
-# add source names to generators
-add_source_types(session, network, table=Source)
-
 network.lines.s_nom = network.lines.s_nom*1.5
 network.transformers.s_nom = network.transformers.s_nom*1.5
 
 # start powerflow calculations
-x=time.time()
-network.lopf(snapshots)
-y=time.time()
-z=(y-x)/60
-network.model.write('/home/ulf/file.lp', io_options={'symbolic_solver_labels':True})
+x = time.time()
+network.lopf(scenario.timeindex, solver_name='gurobi')
+y = time.time()
+z = (y - x) / 60
+
+network.model.write('/home/ulf/file.lp', io_options={'symbolic_solver_labels':
+                                                     True})
 
 # make a line loading plot
 plot_line_loading(network)
 
-#plot stacked sum of nominal power for each generator type and timestep
+# plot stacked sum of nominal power for each generator type and timestep
 plot_stacked_gen(network, resolution="MW")
 
 # same as before, limited to one specific bus
