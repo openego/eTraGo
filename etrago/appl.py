081a2c6bd5462b0c6470127c04997c6988fd7b9e
Martin <martin.soethe@uni-flensburg.de>
2017-06-02 14:58:23 +0200

    Incorporate networkclustering in appl.py

--- etrago/appl.py
+++ etrago/appl.py
@@ -16,27 +16,52 @@ from egopowerflow.tools.io import NetworkScenario
 import time
 from egopowerflow.tools.plot import plot_line_loading, plot_stacked_gen, add_coordinates
 
-session = oedb_session('oedb')
+args = {'network_clustering':False,
+        'db': 'oedb',
+        'gridversion': None,
+        'method': 'lopf',
+        'start_h': 1,
+        'end_h' : 2,
+        'scn_name': 'Status Quo',
+        'ormcls_prefix': 'EgoGridPfHv',
+        'outfile': '/home/ulf/file.lp',
+        'solver': 'gurobi'}
+
+
+session = oedb_session(args['db'])
+
 # additional arguments cfgpath, version, prefix
-scenario = NetworkScenario(session,version='v0.2.10', prefix='EgoPfHv', method='lopf', start_h=1, end_h=2,
-                           scn_name='Status Quo')
+scenario = NetworkScenario(session,
+                           version=args['gridversion'],
+                           prefix=args['ormcls_prefix'],
+                           method=args['method'],
+                           start_h=args['start_h'],
+                           end_h=args['end_h'],
+                           scn_name=args['scn_name'])
+
 network = scenario.build_network()
 
 # add coordinates
 network = add_coordinates(network)
 
-# data preparation
-network.storage_units.p_nom_extendable = True
-network.lines.s_nom = network.lines.s_nom*1.5
-network.transformers.s_nom = network.transformers.s_nom*1.5
+# sh scenario
+if args['scn_name'] == 'SH Status Quo':
+    prep_sh_scenario(network)
+
+# network clustering
+if args['network_clustering']:
+    network.generators.control="PV"
+    busmap = busmap_from_psql(network, session, scn_name=scenario)
+    network = cluster_on_extra_high_voltage(network, busmap, with_time=True)
+
 
 # start powerflow calculations
 x = time.time()
-network.lopf(scenario.timeindex, solver_name='gurobi')
+network.lopf(scenario.timeindex, solver_name=args['solver'])
 y = time.time()
 z = (y - x) / 60
 
-network.model.write('/home/ulf/file.lp', io_options={'symbolic_solver_labels':
+network.model.write(args['outfile'], io_options={'symbolic_solver_labels':
                                                      True})
 
 # make a line loading plot
