3ca671dce0c9be7954b28b37cb4e129314b4bd15
MariusVes <marius@vespermann.de>
2017-09-22 17:01:47 +0200

    Small hotfix for import of kmean_clustering
    The funciton kmean_clustering was moved to a different module some commits ago

--- etrago/appl.py
+++ etrago/appl.py
@@ -20,9 +20,9 @@ from egopowerflow.tools.plot import (plot_line_loading, plot_stacked_gen,
                                      add_coordinates, curtailment, gen_dist,
                                      storage_distribution)
 
-from etrago.extras.utilities import load_shedding, data_manipulation_sh, results_to_csv, parallelisation, pf_post_lopf, loading_minimization, calc_line_losses, kmean_clustering, group_parallel_lines
+from etrago.extras.utilities import load_shedding, data_manipulation_sh, results_to_csv, parallelisation, pf_post_lopf, loading_minimization, calc_line_losses, group_parallel_lines
 
-from etrago.cluster.networkclustering import busmap_from_psql, cluster_on_extra_high_voltage
+from etrago.cluster.networkclustering import busmap_from_psql, cluster_on_extra_high_voltage, kmean_clustering
 from pypsa.networkclustering import get_clustering_from_busmap, busmap_by_kmeans
 import pandas as pd
 
