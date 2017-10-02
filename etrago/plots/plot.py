6a6be214fbb0ac85920ee2850860b9c820070e3e
mariusves <marius@vespermann.de>
2017-10-02 07:53:47 +0200

    add latest version of plot.py from ego.pf

--- etrago/plots/plot.py
+++ etrago/plots/plot.py
@@ -15,6 +15,8 @@ from math import sqrt
 from geoalchemy2.shape import to_shape
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
+import time
 
 
 def add_coordinates(network):
@@ -36,7 +38,8 @@ def add_coordinates(network):
 
     return network
     
-def plot_line_loading(network, timestep=0, filename=None):
+def plot_line_loading(network, timestep=0, filename=None, boundaries=[],
+                      arrows=False):
     """
     Plot line loading as color on lines
 
@@ -54,27 +57,86 @@ def plot_line_loading(network, timestep=0, filename=None):
 
     # calculate relative line loading as S/S_nom
     # with S = sqrt(P^2 + Q^2)
-
+    x = time.time()
+    cmap = plt.cm.jet
     if network.lines_t.q0.empty:
-         loading = abs((network.lines_t.p0.loc[network.snapshots[timestep]]/ \
-                   (network.lines.s_nom)) * 100 )
+        loading_c = (network.lines_t.p0.loc[network.snapshots[timestep]]/ \
+                   (network.lines.s_nom)) * 100 
+        loading = abs(loading_c)
     else:
          loading = ((network.lines_t.p0.loc[network.snapshots[timestep]] ** 2 +
                    network.lines_t.q0.loc[network.snapshots[timestep]] ** 2).\
                    apply(sqrt) / (network.lines.s_nom)) * 100 
 
     # do the plotting
-    ll = network.plot(line_colors=abs(loading), line_cmap=plt.cm.jet,
-                      title="Line loading")
-
+    ll = network.plot(line_colors=abs(loading), line_cmap=cmap,
+                      title="Line loading", line_widths=0.55)
+    
     # add colorbar, note mappable sliced from ll by [1]
-    cb = plt.colorbar(ll[1])
+
+    if not boundaries:
+        cb = plt.colorbar(ll[1])
+    elif boundaries:
+        v = np.linspace(boundaries[0], boundaries[1], 101)
+        cb = plt.colorbar(ll[1], boundaries=v,
+                          ticks=v[0:101:10])
+        cb.set_clim(vmin=boundaries[0], vmax=boundaries[1])
+
     cb.set_label('Line loading in %')
+    
+#==============================================================================
+#     x, y, u, v = np.zeros((4, 10))
+#     path = ll[1].get_segments()
+#     for i in range(0, len(x)):
+#         x[i] = path[i][0][0]
+#         y[i] = path[i][0][1]
+#         u[i] = path[i][1][0] - path[i][0][0]
+#         v[i] = path[i][1][1] - path[i][0][1]
+#     plt.quiver(x, y, u, v, scale=1, units="xy")
+#     plt.axis('equal')
+#     plt.grid()
+#==============================================================================
+    
+    if arrows:
+        ax = plt.axes()
+        path = ll[1].get_segments()
+        x_coords_lines = np.zeros([len(path)])
+        cmap = cmap
+        colors = cmap(ll[1].get_array()/100)
+        for i in range(0, len(path)):
+            x_coords_lines[i] = network.buses.loc[str(network.lines.iloc[i, 2]),'x']
+            color = colors[i]
+            if (x_coords_lines[i] == path[i][0][0] and loading[i] >= 0)\
+                or (x_coords_lines[i] != path[i][0][0] and loading[i] < 0):
+                    arrowprops = dict(arrowstyle="<-", color=color)
+            else:
+                arrowprops = dict(arrowstyle="->", color=color)
+            ax.annotate("",
+                        xy=abs((path[i][0] - path[i][1]) * 0.51 - path[i][0]),
+                        xytext=abs((path[i][0] - path[i][1]) * 0.49 - path[i][0]),
+                        arrowprops=arrowprops,
+                        size=10
+                        )
+    
+#==============================================================================
+#     ax = plt.axes()
+#     for i in range(0, 10):
+#         ax.arrow(x = ll[1].get_segments()[i][0][0],
+#                  y = ll[1].get_segments()[i][0][1],
+#                  dx = ll[1].get_segments()[i][1][0] - ll[1].get_segments()[i][0][0],
+#                  dy = ll[1].get_segments()[i][1][1] - ll[1].get_segments()[i][0][1]
+#                  )
+#==============================================================================
+    
     if filename is None:
         plt.show()
     else:
         plt.savefig(filename)
         plt.close()
+    
+    y = time.time()
+    z = (y-x)/60
+    print(z)
 
 def plot_residual_load(network):
     """ Plots residual load summed of all exisiting buses.
@@ -224,17 +286,15 @@ def storage_distribution(network, filename=None):
         Specify filename
         If not given, figure will be show directly
     """
-    
-    stores = network.storage_units   
-    storage_distribution = network.storage_units.p_nom_opt[stores.index].groupby(network.storage_units.bus).sum().reindex(network.buses.index,fill_value=0.)
+    storage_distribution = network.storage_units.p_nom_opt.groupby(network.storage_units.bus).sum()
 
     fig,ax = plt.subplots(1,1)
     fig.set_size_inches(6,6)
-   
+    
     if sum(storage_distribution) == 0:
          network.plot(bus_sizes=0,ax=ax,title="No extendable storage")
     else:
-         network.plot(bus_sizes=storage_distribution,ax=ax,line_widths=0.3,title="Storage distribution")
+         network.plot(bus_sizes=2*storage_distribution,ax=ax,title="Storage distribution")
     
     if filename is None:
         plt.show()
@@ -244,6 +304,132 @@ def storage_distribution(network, filename=None):
 
 
 def gen_dist(network, techs=None, snapshot=0, n_cols=3,gen_size=0.2, filename=None):
+
+    """
+    Generation distribution
+    ----------
+    network : PyPSA network container
+        Holds topology of grid including results from powerflow analysis
+    techs : dict 
+        type of technologies which shall be plotted
+    snapshot : int
+        snapshot
+    n_cols : int 
+        number of columns of the plot
+    gen_size : num 
+        size of generation bubbles at the buses
+    filename : str
+        Specify filename
+        If not given, figure will be show directly
+    """
+    if techs is None:
+        techs = network.generators.carrier.unique()
+    else:
+        techs = techs
+
+    n_graphs = len(techs)
+    n_cols = n_cols
+
+    if n_graphs % n_cols == 0:
+        n_rows = n_graphs // n_cols
+    else:
+        n_rows = n_graphs // n_cols + 1
+
+    
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols)
+
+    size = 4
+
+    fig.set_size_inches(size*n_cols,size*n_rows)
+
+    for i,tech in enumerate(techs):
+        i_row = i // n_cols
+        i_col = i % n_cols
+    
+        ax = axes[i_row,i_col]
+    
+        gens = network.generators[network.generators.carrier == tech]
+        gen_distribution = network.generators_t.p[gens.index].\
+        loc[network.snapshots[snapshot]].groupby(network.generators.bus).sum().\
+        reindex(network.buses.index,fill_value=0.)
+    
+    
+        network.plot(ax=ax,bus_sizes=gen_size*gen_distribution, line_widths=0.1)
+    
+        ax.set_title(tech)
+    if filename is None:
+       plt.show()
+    else:
+       plt.savefig(filename)
+plt.close()
+
+def gen_dist_diff(networkA, networkB, techs=None, snapshot=0, n_cols=3,gen_size=0.2, filename=None, buscmap=plt.cm.jet):
+
+    """
+    Generation distribution
+    ----------
+    network : PyPSA network container
+        Holds topology of grid including results from powerflow analysis
+    techs : dict 
+        type of technologies which shall be plotted
+    snapshot : int
+        snapshot
+    n_cols : int 
+        number of columns of the plot
+    gen_size : num 
+        size of generation bubbles at the buses
+    filename : str
+        Specify filename
+        If not given, figure will be show directly
+    """
+    if techs is None:
+        techs = networkA.generators.carrier.unique()
+    else:
+        techs = techs
+
+    n_graphs = len(techs)
+    n_cols = n_cols
+
+    if n_graphs % n_cols == 0:
+        n_rows = n_graphs // n_cols
+    else:
+        n_rows = n_graphs // n_cols + 1
+
+    
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols)
+
+    size = 4
+
+    fig.set_size_inches(size*n_cols,size*n_rows)
+
+    for i,tech in enumerate(techs):
+        i_row = i // n_cols
+        i_col = i % n_cols
+    
+        ax = axes[i_row,i_col]
+    
+        gens = networkA.generators[networkA.generators.carrier == tech]
+        gen_distribution = networkA.generators_t.p[gens.index].\
+        loc[networkA.snapshots[snapshot]].groupby(networkA.generators.bus).sum().\
+        reindex(networkA.buses.index,fill_value=0.) - networkB.generators_t.p[gens.index].\
+        loc[networkB.snapshots[snapshot]].groupby(networkB.generators.bus).sum().\
+        reindex(networkB.buses.index,fill_value=0.)
+    
+    
+        networkA.plot(ax=ax,bus_sizes=gen_size*abs(gen_distribution), 
+                      bus_colors=gen_distribution, line_widths=0.1, bus_cmap=buscmap)
+    
+        ax.set_title(tech)
+
+        
+    if filename is None:
+       plt.show()
+    else:
+       plt.savefig(filename)
+plt.close()
+
+
+def gen_dist(network, techs=None, snapshot=1, n_cols=3,gen_size=0.2, filename=None):
 
     """
     Generation distribution
@@ -293,8 +479,8 @@ def gen_dist(network, techs=None, snapshot=0, n_cols=3,gen_size=0.2, filename=No
         gen_distribution = network.generators_t.p[gens.index].\
         loc[network.snapshots[snapshot]].groupby(network.generators.bus).sum().\
         reindex(network.buses.index,fill_value=0.)
+        
     
-   
     
         network.plot(ax=ax,bus_sizes=gen_size*gen_distribution, line_widths=0.1)
     
@@ -305,32 +491,7 @@ def gen_dist(network, techs=None, snapshot=0, n_cols=3,gen_size=0.2, filename=No
        plt.savefig(filename)
        plt.close()
 
-def load_dist(network, snapshot=0, filename=None):
 
-    fig,ax = plt.subplots(1,1)
-    fig.set_size_inches(6,6)
-    load_distribution = network.loads_t.p_set.loc[network.snapshots[snapshot]].groupby(network.loads.bus).sum()
-    load_distribution_q = network.loads_t.q_set.loc[network.snapshots[snapshot]].groupby(network.loads.bus).sum()
-    network.plot(bus_sizes=load_distribution,ax=ax,legend='active load')
-    network.plot(bus_colors='r',bus_sizes=load_distribution_q,ax=ax,legend='reactive load', title="Load distribution")
-    ax.legend(ncol=1,loc="upper left")
-
-    if filename is None:
-       plt.show()
-    else:
-       plt.savefig(filename)
-       plt.close()
-
-def v_mag_minmax(network, filename=None):
-    fig,ax = plt.subplots(1,1)
-    fig.set_size_inches(6,6)
-    network.buses_t.v_mag_pu.min().plot(ax=ax, legend='minimal')
-    network.buses_t.v_mag_pu.max().plot(ax=ax, legend='maximal', title= 'Extreme Voltage Magnitudes (p.u) per bus throughout simulation time' )
-    if filename is None:
-       plt.show()
-    else:
-       plt.savefig(filename)
-       plt.close()
 
     
 if __name__ == '__main__':
