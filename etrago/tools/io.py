3e84f9a56ea8ef63cc041ace569bafaa82c790ef
ClaraBuettner <clarabuettner@aol.com>
2017-11-02 20:03:58 +0100

    Change pypsa_version
    (cherry picked from commit 356e4eb54ea5cc29e1668a5d85ab51ef4ad2cf32)

--- etrago/tools/io.py
+++ etrago/tools/io.py
@@ -221,7 +221,7 @@ class NetworkScenario(ScenarioBase):
 
         timevarying_override = False
 
-        if pypsa.__version__ == '0.8.0':
+        if pypsa.__version__ == '0.11.0':
 
             old_to_new_name = {'Generator':
                                {'p_min_pu_fixed': 'p_min_pu',
