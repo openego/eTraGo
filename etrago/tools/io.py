5204ef9ff1037c4a8a09bce69d1729be424942f7
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2017-11-03 11:04:12 +0100

    Update io.py
    (cherry picked from commit 7975d1889f6bbc92ab45bca46dd054a246b73969)

--- etrago/tools/io.py
+++ etrago/tools/io.py
@@ -524,6 +524,6 @@ def results_to_oedb(session, network, args, grid='hv'):
     
     
 if __name__ == '__main__':
-    if pypsa.__version__ not in ['0.6.2', '0.8.0']:
+    if pypsa.__version__ not in ['0.6.2', '0.11.0']:
         print('Pypsa version %s not supported.' % pypsa.__version__)
     pass
