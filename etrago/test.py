4e1c69d86805705ba66550ac54642310a319b302
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-11-27 16:33:33 +0100

    add appl doc string

--- etrago/test.py
+++ etrago/test.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Test Documentation
+This is the application file for the tool eTraGo. 
 
+Define your connection parameters and power flow settings before executing the function etrago.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation; either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+__copyright__ = "Flensburg University of Applied Sciences, Europa-Universität Flensburg, Centre for Sustainable Energy Systems, DLR-Institute for Networked Energy Systems"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__author__ = "ulfmueller, lukasol, wolfbunke, mariusves, s3pp"
 
 
 import numpy as np
