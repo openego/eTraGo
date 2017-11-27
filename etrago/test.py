ea19362564b3224a28be9dcd3795f2c58455faec
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-11-27 16:09:45 +0100

    doc testing

---
+++ etrago/test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Test Documentation
+
+"""
+
+
+
+import numpy as np
+import pandas as pd
+
+
+def test_function(df):
+    """
+    This is a test function
+    
+    Parameter:
+    ----------
+    
+    df: pandas.DataFrame
+        DataFrame which inclueds something
+        
+    Results:
+    --------
+    
+    df: pandas.DataFrame
+    
+    """
+    
+    
+    
+    
+    
+    return df
+
