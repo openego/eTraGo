cb5abc04e365dc81fd164444c39786a408ea2163
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-11-27 17:01:31 +0100

    add internal import to main

--- etrago/test.py
+++ etrago/test.py
@@ -320,6 +320,14 @@ def etrago(args):
 
 if __name__ == '__main__':
     # Sphinx does not run this code.
+    from etrago.tools.io import NetworkScenario, results_to_oedb
+    from etrago.tools.plot import (plot_line_loading, plot_stacked_gen,
+                                     add_coordinates, curtailment, gen_dist,
+                                     storage_distribution)
+    from etrago.tools.utilities import (oedb_session, load_shedding, data_manipulation_sh,
+                                    results_to_csv, parallelisation, pf_post_lopf, 
+                                    loading_minimization, calc_line_losses, group_parallel_lines)
+    from etrago.cluster.networkclustering import busmap_from_psql, cluster_on_extra_high_voltage, kmean_clustering
     print("run eTraGo automatically")
     network = etrago(args)
 
