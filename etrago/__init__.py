73ee96791cb3e4e941794e87e14366a72b151aaf
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-11-28 09:45:32 +0100

    remove imports

--- etrago/__init__.py
+++ etrago/__init__.py
@@ -6,26 +6,9 @@ eTraGo
 
 """
 
-__version__ = "0.4"
+
 __copyright__ = "Flensburg University of Applied Sciences, Europa-Universität Flensburg, Centre for Sustainable Energy Systems, DLR-Institute for Networked Energy Systems"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "ulfmueller, lukasol, wolfbunke, mariusves, s3pp"
-
-
-#https://stackoverflow.com/questions/33743880/what-does-from-future-import-absolute-import-actually-do
-#from __future__ import absolute_import
-
-#from .tools.io import *
-#from .tools.plot import *
-#from .tools.utiliies import*
-#from .tools.snapshot_clustering import *
-
-#from .cluster.snapshot import prepare_network, linkage, fcluster, get_medoids, update_data_frames
-#from .cluster.networkclustering import cluster_on_extra_high_voltage, graph_from_edges, gen, shortest_path, busmap_by_shortest_path, busmap_from_psql, 
-
-from .cluster.networkclustering import cluster_on_extra_high_voltage
- 
-#import logging
-#logging.basicConfig(level=logging.INFO)
-
+__version__ = "0.4"
 
