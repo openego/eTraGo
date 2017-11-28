d56aebed90991799245f59904b154262c0f9f168
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2017-11-28 09:26:25 +0100

    change imports

--- etrago/__init__.py
+++ etrago/__init__.py
@@ -15,10 +15,10 @@ __author__ = "ulfmueller, lukasol, wolfbunke, mariusves, s3pp"
 #https://stackoverflow.com/questions/33743880/what-does-from-future-import-absolute-import-actually-do
 from __future__ import absolute_import
 
-from .tools.io import *
-from .tools.plot import *
-from .tools.utiliies import*
-from .tools.snapshot_clustering import *
+#from .tools.io import *
+#from .tools.plot import *
+#from .tools.utiliies import*
+#from .tools.snapshot_clustering import *
 
 from .cluster.snapshot import prepare_network, linkage, fcluster, get_medoids, update_data_frames
 from .cluster.networkclustering import cluster_on_extra_high_voltage, graph_from_edges, gen, shortest_path, busmap_by_shortest_path, busmap_from_psql, kmean_clustering
