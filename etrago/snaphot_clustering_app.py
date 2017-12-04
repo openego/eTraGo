f20206bda2b6462bea6ce274577891009553dbf2
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2017-12-04 11:49:46 +0100

    Update snaphot_clustering_app.py

--- etrago/snaphot_clustering_app.py
+++ etrago/snaphot_clustering_app.py
@@ -8,21 +8,18 @@ __author__ = "Simon Hilpert"
 import os
 import pandas as pd
 
-
 import numpy as np
 from numpy import genfromtxt
 np.random.seed()
-from egopowerflow.tools.tools import oedb_session
-from egopowerflow.tools.io import NetworkScenario, results_to_oedb
 import time
-from egopowerflow.tools.plot import (plot_line_loading, plot_stacked_gen,
+from etrago.tools.io import NetworkScenario, results_to_oedb
+from etrago.tools.plot import (plot_line_loading, plot_stacked_gen,
                                      add_coordinates, curtailment, gen_dist,
                                      storage_distribution)
-
-from etrago.extras.utilities import load_shedding, data_manipulation_sh, results_to_csv, parallelisation, pf_post_lopf, loading_minimization, calc_line_losses, group_parallel_lines
-
+from etrago.tools.utilities import (oedb_session, load_shedding, data_manipulation_sh,
+                                    results_to_csv, parallelisation, pf_post_lopf, 
+                                    loading_minimization, calc_line_losses, group_parallel_lines)
 from etrago.cluster.networkclustering import busmap_from_psql, cluster_on_extra_high_voltage, kmean_clustering
-from pypsa.networkclustering import get_clustering_from_busmap, busmap_by_kmeans
 
 from etrago.cluster.snapshot import group, linkage, fcluster, get_medoids
 from pypsa.opf import network_lopf
