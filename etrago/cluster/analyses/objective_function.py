a1aa37a83e3dbe6fc6d0159a6fdf481eaa9441a4
simnh <simon.hilpert@uni-flensburg.de>
2018-01-25 08:59:03 +0100

    Add two axis plot for obj-function vs. run-time difference

--- etrago/cluster/analyses/objective_function.py
+++ etrago/cluster/analyses/objective_function.py
@@ -14,15 +14,34 @@ network = pd.read_csv(path.join(original_path, 'network.csv'))
 
 abs_err = {}
 rel_err = {}
+abs_time = {}
+rel_time = {}
+
 for c in listdir(clustered_path):
     if c != 'Z.csv':
         network_c = pd.read_csv(path.join(clustered_path, c, 'network.csv'))
         abs_err[str(c)] = (abs(network_c['objective'].values[0] -
-                               network['objective'].values[0]))
+                               network['objective'].values[0])) * 100
         rel_err[str(c)] = abs_err[str(c)] / network['objective'].values[0]
+        abs_time[str(c)] = float(network_c['time'])
+        rel_time[str(c)] = (float(network_c['time']) /
+                            float(network['time']) * 100)
 
-errors = pd.DataFrame({'abs_err': abs_err, 'rel_err': rel_err})
-errors.index = [int(i) for i in errors.index]
-errors.sort_index(inplace=True)
+results = pd.DataFrame({
+    'abs_err': abs_err,
+    'rel_err': rel_err,
+    'abs_time': abs_time,
+    'rel_time': rel_time})
+results.index = [int(i) for i in results.index]
+results.sort_index(inplace=True)
 
-errors['rel_err'].plot(style='--*')
+# plotting 2 axis plot
+ax = results['rel_err'].plot(style='--*')
+ax2 = ax.twinx()
+ax.set_title('Comparison')
+ax.set_ylabel('Relative objective function deviation in %')
+ax.set_xlabel('Clustered Days')
+results['rel_time'].plot(ax=ax2, style='*--', color='red')
+ax2.set_ylabel('Relative run-time deviation in %')
+fig = ax.get_figure()
+# fig.savefig("comparison_obj_time.eps")
