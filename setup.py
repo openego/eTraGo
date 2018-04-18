ab7b0eea43054f587124e43b97dbf812c2cb4292
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-04-18 15:31:28 +0200

    update setup change dependency_links

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
       include_package_data=True,
       install_requires=['egoio == 0.3.0',
                         'scikit-learn == 0.19.0',
-		        'pandas >= 0.19.0, <=0.20.3',
+		                'pandas >= 0.19.0, <=0.20.3',
                         'pypsa==0.11.0fork',
                         'sqlalchemy >= 1.0.15, <= 1.1.4',
                         'geoalchemy2 >= 0.3.0, <=0.4.0',
