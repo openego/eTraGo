71a51034eec68a81186b1c784065ec68107c1f98
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2018-04-20 15:24:59 +0200

    minor documentation

--- etrago/appl.py
+++ etrago/appl.py
@@ -57,15 +57,15 @@ args = {# Setup and Configuration:
         'solver': 'gurobi', # glpk, cplex or gurobi
         'scn_name': 'NEP 2035', # state which scenario you want to run: Status Quo, NEP 2035, eGo100
             # Scenario variations:
-            'scn_extension': None, # None or name of additional scenario (in extension_tables)
-            'scn_decommissioning': None, # None or name of decommissioning-scenario (in extension_tables)
-            'add_Belgium_Norway': False,  # state if you want to add Belgium and Norway as electrical neighbours, timeseries from scenario NEP 2035!
+            'scn_extension': 'nep2035_b2', # None or name of additional scenario (in extension_tables) e.g. 'nep2035_b2'
+            'scn_decommissioning': 'nep2035_b2', # None or name of decommissioning-scenario (in extension_tables) e.g. 'nep2035_b2'
+            'add_Belgium_Norway': True,  # state if you want to add Belgium and Norway as electrical neighbours, timeseries from scenario NEP 2035!
         # Export options:
         'lpfile': False, # state if and where you want to save pyomo's lp file: False or /path/tofolder
         'results': False, # state if and where you want to save results as csv: False or /path/tofolder
         'export': False, # state if you want to export the results back to the database
         # Settings:
-        'extendable': None, # None or which components you want to optimize (e.g. 'network')
+        'extendable': True, # None or which components you want to optimize (e.g. 'network')
         'storage_extendable':True, # state if you want storages to be installed at each node if necessary.
         'generator_noise':True, # state if you want to apply a small generator noise 
         'reproduce_noise': False,# state if you want to use a predefined set of random noise for the given scenario. if so, provide path, e.g. 'noise_values.csv'
