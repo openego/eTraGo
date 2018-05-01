f53ce5112ab83e3cfe61c81900248638784959af
Stephan Günther <gnn.code@gmail.com>
2018-05-01 18:35:34 +0200

    Always use `pnb.index` when distributing
    It's a safe bet, as `pnb.index` should cover all parial network buses.

--- etrago/cluster/disaggregation.py
+++ etrago/cluster/disaggregation.py
@@ -401,19 +401,17 @@ class UniformDisaggregation(Disaggregation):
                             pn_t['p_max_pu'].loc[:, pnb.index])
                     psum = p_nom_times_p_max_pu.sum(axis='columns')
                     loc = (slice(None, None),)
-                    index = pnb.index
                 else:
                     p_nom_times_p_max_pu = (
                             pnb.loc[:, 'p_nom'] *
                             pnb.loc[:, 'p_max_pu'])
                     psum = p_nom_times_p_max_pu.sum(axis='index')
                     loc = ()
-                    index = p_nom_times_p_max_pu.index
 
                 # for s in series:
                 for s in ['p']:
                     clt = cl_t[s].loc[:, next(clb.itertuples()).Index]
-                    for bus_id in index:
+                    for bus_id in pnb.index:
                         # TODO: Check whether series multiplication works as
                         #       expected.
                         pn_t[s].loc[:, bus_id] = (
