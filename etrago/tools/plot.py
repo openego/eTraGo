c337a0f5330aadce6cc3a0909453007712121db3
Stephan Günther <gnn.code@gmail.com>
2018-05-01 23:05:07 +0200

    Break overly long line

--- etrago/tools/plot.py
+++ etrago/tools/plot.py
@@ -502,7 +502,11 @@ def storage_distribution(network, filename=None):
     if sum(storage_distribution) == 0:
          network.plot(bus_sizes=0,ax=ax,title="No storages")
     else:
-         network.plot(bus_sizes=storage_distribution,ax=ax,line_widths=0.3,title="Storage distribution")
+         network.plot(
+                 bus_sizes=storage_distribution,
+                 ax=ax,
+                 line_widths=0.3,
+                 title="Storage distribution")
     
     if filename is None:
         plt.show()
