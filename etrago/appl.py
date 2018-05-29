db1681f04eaba2b0389debaec5c68cd161de6c21
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-05-29 15:03:40 +0200

    change header

--- etrago/appl.py
+++ etrago/appl.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
+## Copyright 2016-2018  Flensburg University of Applied Sciences,
+##                      Europa-Universität Flensburg,
+##                      Centre for Sustainable Energy Systems,
+##                      DLR-Institute for Networked Energy Systems
+
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+
+## You should have received a copy of the GNU Affero General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# File description
 """
 This is the application file for the tool eTraGo.
-
 Define your connection parameters and power flow settings before executing the function etrago.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation; either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+"""
 
 __copyright__ = "Flensburg University of Applied Sciences, Europa-Universität Flensburg, Centre for Sustainable Energy Systems, DLR-Institute for Networked Energy Systems"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "ulfmueller, lukasol, wolfbunke, mariusves, s3pp"
-"""
+
 import numpy as np
 from numpy import genfromtxt
 np.random.seed()
@@ -32,14 +37,14 @@ import os
 
 if not 'READTHEDOCS' in os.environ:
     # Sphinx does not run this code.
-    # Do not import internal packages directly  
+    # Do not import internal packages directly
     from etrago.tools.io import NetworkScenario, results_to_oedb, extension, decommissioning
     from etrago.tools.plot import (plot_line_loading, plot_stacked_gen,
                                      add_coordinates, curtailment, gen_dist,
                                      storage_distribution, storage_expansion, extension_overlay_network)
 
     from etrago.tools.utilities import (load_shedding, data_manipulation_sh, convert_capital_costs,
-                                    results_to_csv, parallelisation, pf_post_lopf, 
+                                    results_to_csv, parallelisation, pf_post_lopf,
                                     loading_minimization, calc_line_losses, group_parallel_lines)
     from etrago.tools.extendable import extendable
     from etrago.cluster.networkclustering import busmap_from_psql, cluster_on_extra_high_voltage, kmean_clustering
@@ -66,7 +71,7 @@ args = {# Setup and Configuration:
         'export': False, # state if you want to export the results back to the database
         # Settings:
         'extendable':['storages'], # None or array of components you want to optimize (e.g. ['network', 'storages'])
-        'generator_noise':True, # state if you want to apply a small generator noise 
+        'generator_noise':True, # state if you want to apply a small generator noise
         'reproduce_noise': False,# state if you want to use a predefined set of random noise for the given scenario. if so, provide path, e.g. 'noise_values.csv'
         'minimize_loading':False,
         # Clustering:
@@ -117,20 +122,20 @@ def etrago(args):
     end_snapshot : int
         2,
         End hour of the scenario year to be calculated.
-        
+
     solver : str
-        'glpk', 
+        'glpk',
         Choose your preferred solver. Current options: 'glpk' (open-source),
         'cplex' or 'gurobi'.
-                
+
     scn_name : str
     	'Status Quo',
-	Choose your scenario. Currently, there are three different 
-	scenarios: 'Status Quo', 'NEP 2035', 'eGo100'. If you do not 
-	want to use the full German dataset, you can use the excerpt of 
-	Schleswig-Holstein by adding the acronym SH to the scenario 
+	Choose your scenario. Currently, there are three different
+	scenarios: 'Status Quo', 'NEP 2035', 'eGo100'. If you do not
+	want to use the full German dataset, you can use the excerpt of
+	Schleswig-Holstein by adding the acronym SH to the scenario
 	name (e.g. 'SH Status Quo').
-        
+
    scn_extension : str
        None,
        Choose an extension-scenario which will be added to the existing
@@ -139,48 +144,48 @@ def etrago(args):
        Currently there are two overlay networks:
            'nep2035_confirmed' includes all planed new lines confirmed by the Bundesnetzagentur
            'nep2035_b2' includes alles new lines planned by the Netzentwicklungsplan 2025 in scenario 2035 B2
-           
+
     scn_decommissioning : str
-        None, 
-        Choose an extra scenario which includes lines you want to decommise from the 
-        existing network. Data of the decommissioning scenarios are located in extension-tables 
+        None,
+        Choose an extra scenario which includes lines you want to decommise from the
+        existing network. Data of the decommissioning scenarios are located in extension-tables
         (e.g. model_draft.ego_grid_pf_hv_extension_bus) with the prefix 'decommissioning_'.
-        Currently, there are two decommissioning_scenarios which are linked to extension-scenarios:   
+        Currently, there are two decommissioning_scenarios which are linked to extension-scenarios:
             'nep2035_confirmed' includes all lines that will be replaced in confirmed projects
             'nep2035_b2' includes all lines that will be replaced in NEP-scenario 2035 B2
-            
+
     add_Belgium_Norway : bool
-        False, 
+        False,
         State if you want to add Belgium and Norway as electrical neighbours.
         Currently, generation and load always refer to scenario 'NEP 2035'.
-            
+
     lpfile : obj
-        False, 
+        False,
         State if and where you want to save pyomo's lp file. Options:
         False or '/path/tofolder'.import numpy as np
-        
+
     results : obj
-        False, 
-        State if and where you want to save results as csv files.Options: 
+        False,
+        State if and where you want to save results as csv files.Options:
         False or '/path/tofolder'.
-        
+
     export : bool
-        False, 
-        State if you want to export the results of your calculation 
+        False,
+        State if you want to export the results of your calculation
         back to the database.
-        
+
     extendable : NoneType or list
         ['network', 'storages'],
         Choose None or which components you want to optimize.
-        Settings can be added in /tools/extendable.py. 
+        Settings can be added in /tools/extendable.py.
         The most important possibilities:
             'network': set all lines, links and transformers extendable
             'transformers': set all transformers extendable
             'overlay_network': set all components of the 'scn_extension' extendable
-            'storages': allow to install extendable storages (unlimited in size) 
-                        at each grid node in order to meet the flexibility demand. 
-      
-        
+            'storages': allow to install extendable storages (unlimited in size)
+                        at each grid node in order to meet the flexibility demand.
+
+
     generator_noise : bool
         True,
         Choose if you want to apply a small random noise to the marginal
@@ -209,12 +214,12 @@ def etrago(args):
         Choose if you want to cluster the full HV/EHV dataset down to only the EHV
         buses. In that case, all HV buses are assigned to their closest EHV sub-station,
         taking into account the shortest distance on power lines.
-	
+
     snapshot_clustering : bool or int
     	False,
 	State if you want to cluster the snapshots and run the optimization only on a
 	subset of snapshot periods. The int value defines the number of periods (i.e. days)
-	wich will be clustered to. 
+	wich will be clustered to.
 
     parallelisation : bool
         False,
@@ -269,8 +274,8 @@ def etrago(args):
 
     # add coordinates
     network = add_coordinates(network)
-      
-    # TEMPORARY vague adjustment due to transformer bug in data processing     
+
+    # TEMPORARY vague adjustment due to transformer bug in data processing
     if args['gridversion'] == 'v0.2.11':
         network.transformers.x=network.transformers.x*0.0001
 
@@ -293,7 +298,7 @@ def etrago(args):
             # add random noise to all generator
             network.generators.marginal_cost = noise_values
 
-   
+
     # for SH scenario run do data preperation:
     if args['scn_name'] == 'SH Status Quo' or args['scn_name'] == 'SH NEP 2035':
         data_manipulation_sh(network)
@@ -312,39 +317,39 @@ def etrago(args):
     if not args['network_clustering_kmeans'] == False:
         network = kmean_clustering(network, n_clusters=args['network_clustering_kmeans'],
                                    load_cluster=args['load_cluster'],
-                                   line_length_factor= 1.25, remove_stubs=True, 
-                                   use_reduced_coordinates=False, bus_weight_tocsv=None, 
+                                   line_length_factor= 1.25, remove_stubs=True,
+                                   use_reduced_coordinates=False, bus_weight_tocsv=None,
                                    bus_weight_fromcsv=None)
 
     # Branch loading minimization
     if args['minimize_loading']:
         extra_functionality = loading_minimization
-    
+
     if args['skip_snapshots']:
         network.snapshots=network.snapshots[::args['skip_snapshots']]
-        network.snapshot_weightings=network.snapshot_weightings[::args['skip_snapshots']]*args['skip_snapshots']   
-        
+        network.snapshot_weightings=network.snapshot_weightings[::args['skip_snapshots']]*args['skip_snapshots']
+
     if args ['scn_extension'] != None:
          network = extension(network, session, scn_extension = args ['scn_extension'],start_snapshot=args['start_snapshot'], end_snapshot=args['end_snapshot'], k_mean_clustering = args['network_clustering_kmeans'])
-    
+
     if args ['scn_decommissioning'] != None:
          network = decommissioning(network, session, scn_decommissioning = args ['scn_decommissioning'],k_mean_clustering = args['network_clustering_kmeans'])
 
     if args ['add_Belgium_Norway']:
          network = extension(network, session, scn_extension = 'BE_NO_NEP 2035', start_snapshot=args['start_snapshot'], end_snapshot=args['end_snapshot'], k_mean_clustering = args['network_clustering_kmeans'])
-        
+
     if args ['extendable'] != None:
         network = extendable(network, args['extendable'], args ['scn_extension'])
         network = convert_capital_costs(network, args['start_snapshot'], args['end_snapshot'])
-    
+
     if args['branch_capacity_factor']:
         network.lines.s_nom = network.lines.s_nom*args['branch_capacity_factor']
         network.transformers.s_nom = network.transformers.s_nom*args['branch_capacity_factor']
-        
+
      #load shedding in order to hunt infeasibilities
     if args['load_shedding']:
         load_shedding(network)
-        
+
     # snapshot clustering
     if not args['snapshot_clustering']== False:
         network = snapshot_clustering(network, how='daily', clusters= args['snapshot_clustering'])
@@ -352,9 +357,9 @@ def etrago(args):
 
     # parallisation
     if args['parallelisation']:
-        parallelisation(network, start_snapshot=args['start_snapshot'], 
-                        end_snapshot=args['end_snapshot'],group_size=1, 
-                        solver_name=args['solver'], 
+        parallelisation(network, start_snapshot=args['start_snapshot'],
+                        end_snapshot=args['end_snapshot'],group_size=1,
+                        solver_name=args['solver'],
                         extra_functionality=extra_functionality)
     # start linear optimal powerflow calculations
     elif args['method'] == 'lopf':
@@ -362,18 +367,18 @@ def etrago(args):
 
         network.lopf(network.snapshots, solver_name=args['solver'], extra_functionality=extra_functionality)
         y = time.time()
-        z = (y - x) / 60 
+        z = (y - x) / 60
         print("Time for LOPF [min]:",round(z,2))# z is time for lopf in minutes
 
             # start non-linear powerflow simulation
     elif args['method'] == 'pf':
             network.pf(scenario.timeindex)
             # calc_line_losses(network)
-        
+
     if args['pf_post_lopf']:
         pf_post_lopf(network, scenario)
         calc_line_losses(network)
-    
+
 
        # provide storage installation costs
     if sum(network.storage_units.p_nom_opt) != 0:
@@ -418,4 +423,3 @@ if __name__ == '__main__':
     # plot to show extendable storages
     #storage_distribution(network)
     #extension_overlay_network(network)
-
