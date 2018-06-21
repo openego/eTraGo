cb6359e1ceb3ecf4151f258ae98e3177b50d8f91
WolfBunke <wolf-dieter.bunke@uni-flensburg.de>
2018-06-21 11:15:50 +0200

    update header add license

--- etrago/cluster/snapshot.py
+++ etrago/cluster/snapshot.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+## Copyright 2016-2018  Flensburg University of Applied Sciences,
+##                      Europa-Universität Flensburg,
+##                      Centre for Sustainable Energy Systems
+
+
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+ 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+ 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# File description for read-the-docs
 """ This module contains functions for calculating representative days/weeks
 based on a pyPSA network object. It is designed to be used the the `lopf`
 method.
@@ -14,8 +33,9 @@ Remaining questions/tasks:
 - Include scaling method for yearly sums
 """
 
-__copyright__ = "tba"
-__license__ = "tba"
+__copyright__ = "Flensburg University of Applied Sciences, \
+                 Europa-Universität Flensburg, Centre for Sustainable Energy Systems"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "Simon Hilpert"
 
 import pandas as pd
