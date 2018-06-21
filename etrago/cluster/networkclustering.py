aaaad0f5d523714841264ef16f4ece9b33cc873e
WolfBunke <wolf-dieter.bunke@uni-flensburg.de>
2018-06-21 11:13:32 +0200

    update header

--- etrago/cluster/networkclustering.py
+++ etrago/cluster/networkclustering.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
+## Copyright 2016-2018  Flensburg University of Applied Sciences,
+##                      Europa-Universität Flensburg,
+##                      Centre for Sustainable Energy Systems,
+##                      DLR-Institute for Networked Energy Systems
+
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+ 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+ 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# File description for read-the-docs
 """ Networkclustering.py defines the methods to cluster power grid
 networks for application within the tool eTraGo. Many of the functions are
 based or taken from 
 https://github.com/PyPSA/PyPSA/blob/master/pypsa/networkclustering.py
 This applies especially for the k-means clustering algorithm. The method is 
 based on Hoersch et al. ( https://arxiv.org/pdf/1705.07617.pdf ).
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation; either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__copyright__ = "Flensburg University of Applied Sciences, Europa-Universität Flensburg, Centre for Sustainable Energy Systems, DLR-Institute for Networked Energy Systems"
+__copyright__ = "Flensburg University of Applied Sciences, \
+                 Europa-Universität Flensburg, Centre for Sustainable Energy Systems,\
+                 DLR-Institute for Networked Energy Systems"
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "s3pp, wolfbunke, ulfmueller, lukasol"
 
