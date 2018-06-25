a2d39dc4fd698de9a5d046d97afe435ea8fa68cb
wolfbunke <wolf-dieter.bunke@uni-flensburg.de>
2018-06-25 16:45:10 +0200

    update pep8

--- etrago/tools/extendable.py
+++ etrago/tools/extendable.py
@@ -3,17 +3,17 @@
 # Europa-Universität Flensburg,
 # Centre for Sustainable Energy Systems,
 # DLR-Institute for Networked Energy Systems
-
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation; either version 3 of the
 # License, or (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,6 +21,7 @@
 """
 Extendable.py defines function to set PyPSA-components extendable.
 """
+from etrago.tools.utilities import set_line_costs, set_trafo_costs
 
 __copyright__ = ("Flensburg University of Applied Sciences,
                  Europa-Universität Flensburg,
@@ -28,8 +29,6 @@ __copyright__ = ("Flensburg University of Applied Sciences,
                  DLR-Institute for Networked Energy Systems")
 __license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
 __author__ = "ulfmueller, s3pp, wolfbunke, mariusves, lukasol"
-
-from etrago.tools.utilities import set_line_costs, set_trafo_costs
 
 
 def extendable(network, extendable, overlay_scn_name=None):
@@ -76,9 +75,12 @@ def extendable(network, extendable, overlay_scn_name=None):
 
     if 'NEP Zubaunetz' in extendable:
         network.lines.loc[(network.lines.project != 'EnLAG') & (
-            network.lines.scn_name == 'extension_' + overlay_scn_name), 's_nom_extendable'] = True
+            network.lines.scn_name == 'extension_' + overlay_scn_name),
+            's_nom_extendable'] = True
         network.transformers.loc[(network.transformers.project != 'EnLAG') & (
-            network.transformers.scn_name == ('extension_' + overlay_scn_name)), 's_nom_extendable'] = True
+            network.transformers.scn_name == ('extension_'
+                                              + overlay_scn_name)),
+                                 's_nom_extendable'] = True
         network.links.loc[network.links.scn_name == (
             'extension_' + overlay_scn_name), 'p_nom_extendable'] = True
 
@@ -96,6 +98,7 @@ def extendable(network, extendable, overlay_scn_name=None):
         network.links.loc[network.links.scn_name == (
             'extension_' + overlay_scn_name), 'p_nom_extendable'] = True
         network.lines.loc[network.lines.scn_name == (
-            'extension_' + overlay_scn_name), 'capital_cost'] = network.lines.capital_cost + (2 * 14166)
+            'extension_' + overlay_scn_name),
+            'capital_cost'] = network.lines.capital_cost + (2 * 14166)
 
     return network
