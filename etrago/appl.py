f44f59445bb2459523c8650eb063f20081ee34b9
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2019-09-23 13:42:30 +0200

    change random noise

--- etrago/appl.py
+++ etrago/appl.py
@@ -430,10 +430,13 @@ def etrago(args):
         s = np.random.RandomState(args['generator_noise'])
         network.generators.marginal_cost[network.generators.bus.isin(
                 network.buses.index[network.buses.country_code == 'DE'])] += \
-            abs(s.normal(0, 0.1, len(network.generators.marginal_cost[
+            abs(s.normal(0, 0.01, len(network.generators.marginal_cost[
                     network.generators.bus.isin(network.buses.index[
                             network.buses.country_code == 'DE'])])))
-    # for SH scenario run do data preperation:
+        network.generators.marginal_cost[network.generators.bus.isin(
+                network.buses.index[network.buses.country_code != 'DE'])] +=0.01        
+        
+        # for SH scenario run do data preperation:
     if (args['scn_name'] == 'SH Status Quo' or
             args['scn_name'] == 'SH NEP 2035'):
         data_manipulation_sh(network)
