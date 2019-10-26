a18572f846243cfcf7f6b859a5259b3a4ac17a11
lukasoldi <lukas.wienholt@posteo.de>
2019-10-26 12:03:25 +0200

    make pypsa 13 ready back again

--- etrago/cluster/networkclustering.py
+++ etrago/cluster/networkclustering.py
@@ -126,7 +126,7 @@ def cluster_on_extra_high_voltage(network, busmap, with_time=True):
         io.import_series_from_dataframe(network_c, df, 'Generator', attr)
 
     # dealing with all other components
-    aggregate_one_ports = components.one_port_components.copy()
+    aggregate_one_ports = network.one_port_components.copy()
     aggregate_one_ports.discard('Generator')
 
     for one_port in aggregate_one_ports:
@@ -499,7 +499,7 @@ def kmean_clustering(network, n_clusters=10, load_cluster=False,
         network.determine_network_topology()
         busmap = busmap_by_stubs(network)
         network.generators['weight'] = network.generators['p_nom']
-        aggregate_one_ports = components.one_port_components.copy()
+        aggregate_one_ports = network.one_port_components.copy()
         aggregate_one_ports.discard('Generator')
         # reset coordinates to the new reduced guys, rather than taking an
         # average (copied from pypsa.networkclustering)
@@ -566,7 +566,7 @@ def kmean_clustering(network, n_clusters=10, load_cluster=False,
 
     # ToDo change function in order to use bus_strategies or similar
     network.generators['weight'] = network.generators['p_nom']
-    aggregate_one_ports = components.one_port_components.copy()
+    aggregate_one_ports = network.one_port_components.copy()
     aggregate_one_ports.discard('Generator')
     clustering = get_clustering_from_busmap(
         network,
