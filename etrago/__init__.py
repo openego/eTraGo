9e7b58f2857de277cbd7acaff511a4cd6e5245ec
Wilko Heitkoetter <wilkohe@torch>
2020-12-23 09:23:26 +0100

    add init file, acidentally deleted before

---
+++ etrago/__init__.py
@@ -0,0 +1,15 @@
+"""
+eTraGo
+******
+- Optimization of flexibility options for transmission grids based on PyPSA
+
+
+"""
+
+
+__copyright__ = "Flensburg University of Applied Sciences, Europa-Universität Flensburg, Centre for Sustainable Energy Systems, DLR-Institute for Networked Energy Systems"
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__author__ = "ulfmueller, lukasol, wolfbunke, mariusves, s3pp"
+__version__ = "0.4"
+
+from etrago.tools.network import Etrago
