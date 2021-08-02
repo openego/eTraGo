365571496a6f7f77c496fb8ff5fc05bd7b963eae
KathiEsterl <katharinaesterl@web.de>
2021-08-02 20:31:44 +0200

    added functionality to test ehv-clustering

--- etrago/cluster/networkclustering.py
+++ etrago/cluster/networkclustering.py
@@ -257,7 +257,7 @@ def shortest_path(paths, graph):
             df_isna = df.isnull()
             
     df.to_csv('df_neu.csv') ###
-
+    
     return df
 
 
@@ -359,14 +359,14 @@ def busmap_by_shortest_path(etrago, scn_name, fromlvl, tolvl, cpu_cores=4):
     df = pd.concat([df, tofill], ignore_index=True, axis=0)
 
     ###
-    '''# prepare data for export
+    # prepare data for export
 
-    df['scn_name'] = scn_name
+    '''df['scn_name'] = scn_name
     df['version'] = etrago.args['gridversion']'''
 
     df.rename(columns={'source': 'bus0', 'target': 'bus1'}, inplace=True)
+    df=df.to_dict()
     '''df.set_index(['scn_name', 'bus0', 'bus1'], inplace=True)
-    
     
     for i, d in df.reset_index().iterrows():
         etrago.session.add(EgoGridPfHvBusmap(**d.to_dict()))
@@ -409,11 +409,11 @@ def busmap_from_psql(etrago):
 
         return dict(query.all())
 
-    #busmap = fetch() ###
+    busmap = fetch() ###
 
     # TODO: Or better try/except/finally
-    if True: ###
-    #if not busmap:
+    #if True: ###
+    if not busmap:
         print('Busmap does not exist and will be created.\n')
 
         cpu_cores = mp.cpu_count() #input('cpu_cores (default 4): ') or '4' ###
