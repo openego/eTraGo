2401c62ee473026f1fa33998e03020cc4a0a4f75
KathiEsterl <katharinaesterl@web.de>
2021-08-02 22:31:11 +0200

    changed to examine ehv-clustering for tests

--- etrago/cluster/networkclustering.py
+++ etrago/cluster/networkclustering.py
@@ -410,7 +410,7 @@ def busmap_from_psql(etrago):
         return dict(query.all())
 
     busmap_online = fetch() ### ohne online
-    busmap_online.to_csv('busmap_online.csv') ###
+    pd.DataFrame.from_dict(busmap_online) ###
 
     # TODO: Or better try/except/finally
     if True: ###
@@ -423,7 +423,7 @@ def busmap_from_psql(etrago):
         busmap = busmap_by_shortest_path(etrago, scn_name,
                                 fromlvl=[110], tolvl=[220, 380, 400, 450],
                                 cpu_cores=int(cpu_cores))
-        busmap.to_csv('busmap_neu.csv') ###
+        pd.DataFrame.from_dict(busmap_neu) ###
         #busmap = fetch()
 
     return busmap
