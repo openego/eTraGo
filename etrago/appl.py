048322aa0e4223d5c5bd0dc1fa6850a85d6d06bf
AmeliaNadal <nada_am@torch>
2022-02-07 17:31:39 +0100

    Adapt description of n_clusters_gas

--- etrago/appl.py
+++ etrago/appl.py
@@ -75,7 +75,7 @@ args = {
     'network_clustering_kmeans': {
         'active': True, # choose if clustering is activated
         'n_clusters': 20, # number of resulting nodes
-        'n_clusters_gas': 5, # number of resulting nodes
+        'n_clusters_gas': 5, # number of resulting nodes in Germany
         'kmeans_busmap': False, # False or path/to/busmap.csv
         'line_length_factor': 1, #
         'remove_stubs': False, # remove stubs bevore kmeans clustering
