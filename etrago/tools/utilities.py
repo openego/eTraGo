65dd7f313aefbbd0c3092ce3a4cea60ee90279b5
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2022-05-12 17:42:45 +0200

    gas generators switch and generators scale down

--- etrago/tools/utilities.py
+++ etrago/tools/utilities.py
@@ -1622,6 +1622,39 @@ def drop_sectors(self, drop_carriers):
     None.
 
     """
+    
+    if self.scenario.scn_name == 'eGon2035':
+
+        if 'CH4' in drop_carriers:
+            # create gas generators from links in order to not lose them when dropping non-electric carriers
+            gas_to_add = ['central_gas_CHP', 'industrial_gas_CHP']
+            gen = self.network.generators
+            
+            for i in gas_to_add:
+                gen_empty = gen.drop(gen.index)
+                gen_empty.bus = self.network.links[self.network.links.carrier == i].bus1
+                gen_empty.p_nom = self.network.links[self.network.links.carrier == i].p_nom
+                gen_empty.marginal_cost = self.network.links[self.network.links.carrier == i].marginal_cost
+                gen_empty.carrier = i
+                gen_empty.scn_name = 'eGon2035'
+                gen_empty.p_nom_extendable = False
+                gen_empty.sign = 1
+                gen_empty.p_min_pu = 0
+                gen_empty.p_max_pu = 1
+                gen_empty.control = 'PV'
+                gen_empty.fillna(0, inplace=True)
+                self.network.generators= self.network.generators.append(gen_empty, verify_integrity=True)
+                
+        if 'H2_ind_load' and'central_heat' and 'Li ion' and 'rural_heat' in drop_carriers:
+            # scale down generation facilities with respect to dropping additional loads
+            scale_down = 518.2/633.8
+        
+            gens_to_scale = ['solar', 'wind_offshore', 'wind_onshore', 'solar_rooftop', 'biomass']
+            foreign_index = self.network.buses[self.network.buses.country !='DE'].index
+            self.network.generators.p_nom[
+                (~self.network.generators.index.isin(foreign_index))|
+                (self.network.generators.carrier.isin(gens_to_scale))] *= scale_down
+    
 
     self.network.mremove('Bus',
         self.network.buses[
