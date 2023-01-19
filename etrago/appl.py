ecce2bdbfde83897474faec959d323f1c0715130
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-01-19 16:27:06 +0100

    Set cyclic soc for all stores

--- etrago/appl.py
+++ etrago/appl.py
@@ -591,10 +591,10 @@ def run_etrago(args, json_path):
 
     etrago.spatial_clustering_gas()
 
-    #etrago.args["load_shedding"] = True
-    #etrago.load_shedding()
+    etrago.args["load_shedding"] = True
+    etrago.load_shedding()
 
-    etrago.network.stores.e_cyclic[etrago.network.stores.carrier.str.contains('heat')] = True
+    etrago.network.stores.e_cyclic = True
     etrago.network.storage_units.cyclic_state_of_charge = True
 
     # snapshot clustering
