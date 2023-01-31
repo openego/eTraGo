312dbbbe64c813ad4b15f91d161934b626cc4233
tillsilke <till.silkenath@gmail.com>
2023-01-31 14:42:03 +0100

    appl.py vergessen

--- etrago/appl.py
+++ etrago/appl.py
@@ -81,7 +81,7 @@ args = {
     "extra_functionality": {},  # Choose function name or {}
     # Spatial Complexity:
     "network_clustering": {
-        "random_state": 42,  # random state for replicability of kmeans results
+        "random_state": 30,  # random state for replicability of kmeans results
         "active": True,  # choose if clustering is activated
         "method": "kmedoids-dijkstra",  # choose clustering method: kmeans or kmedoids-dijkstra
         "n_clusters_AC": 30,  # total number of resulting AC nodes (DE+foreign)
