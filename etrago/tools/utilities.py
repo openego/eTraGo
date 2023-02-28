dcf6aa671322c4910194bf7c8ca6727988eb97fe
Katharina Esterl <katharinaesterl@web.de>
2023-02-28 15:41:33 +0100

    Change the way new generators are added to network

--- etrago/tools/utilities.py
+++ etrago/tools/utilities.py
@@ -2324,8 +2324,8 @@ def drop_sectors(self, drop_carriers):
     ----------
     drop_carriers : array
         List of sectors that will be dropped.
-        e.g. ['dsm', 'CH4', 'H2_saltcavern', 'H2_grid', 
-              'central_heat', 'rural_heat', 'central_heat_store', 
+        e.g. ['dsm', 'CH4', 'H2_saltcavern', 'H2_grid',
+              'central_heat', 'rural_heat', 'central_heat_store',
               'rural_heat_store', 'Li ion'] means everything but AC
 
     Returns
@@ -2336,7 +2336,7 @@ def drop_sectors(self, drop_carriers):
 
     if self.scenario.scn_name == "eGon2035":
         if "CH4" in drop_carriers:
-            # create gas generators from links 
+            # create gas generators from links
             # in order to not lose them when dropping non-electric carriers
             gas_to_add = ["central_gas_CHP", "industrial_gas_CHP", "OCGT"]
             gen = self.network.generators
@@ -2367,8 +2367,8 @@ def drop_sectors(self, drop_carriers):
                 gen_empty.p_max_pu = 1
                 gen_empty.control = "PV"
                 gen_empty.fillna(0, inplace=True)
-                self.network.generators = self.network.generators.append(
-                    gen_empty, verify_integrity=True
+                self.network.import_components_from_dataframe(
+                    gen_empty, "Generator"
                 )
 
     self.network.mremove(
