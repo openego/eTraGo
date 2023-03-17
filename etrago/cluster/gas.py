c5fcf3cb6e9ff6ad7c918128b267d23b43541296
Tetens <Hendrik-Pieter.Tetens@dlr.de>
2023-03-17 08:41:34 +0100

    update

--- etrago/cluster/gas.py
+++ etrago/cluster/gas.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+# Copyright 2016-2023 Flensburg University of Applied Sciences,
+# Europa-Universität Flensburg,
+# Centre for Sustainable Energy Systems,
+# DLR-Institute for Networked Energy Systems
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # File description for read-the-docs
 """ gas.py defines the methods to cluster gas grid networks
 spatially for applications within the tool eTraGo."""
