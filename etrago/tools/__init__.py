f00d49aa9c6dd0a89364cc30fd541c2ef257d387
Stephan Günther <gnn.code@gmail.com>
2023-03-24 01:45:17 +0100

    Add some information to "etrago/tools/__init__.py"
    A short docstring along with author, copyright and license information.

--- etrago/tools/__init__.py
+++ etrago/tools/__init__.py
@@ -1,8 +1,12 @@
-"""
+"""Multi purpose tools that don't fit anywhere else in eTraGo.
 """
 
-__copyright__ = "tba"
-__license__ = "tba"
-__author__ = "tba"
+__copyright__ = (
+    "Copyright (C) 2023"
+    " Otto-von-Guericke-University Magdeburg,"
+    " Research group for theoretical computer science"
+)
+__license__ = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__author__ = "gnn <gnn.code@gmail.com>"
 
 
