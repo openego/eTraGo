25ec6a9373f6e5013644716e096c4f693cb4fc97
Katharina Esterl <katharinaesterl@web.de>
2023-04-18 08:12:43 +0200

    Add new resolution, adapt some plots

--- etrago/dijkstra-analysis.py
+++ etrago/dijkstra-analysis.py
@@ -76,7 +76,7 @@ def foreign_sto_expansion(network):
     
 ###############################################################################
 
-spatial_resolution = [20, 30, 40, 50, 100, 150, 200, 250, 300, 400, 500]
+spatial_resolution = [20, 30, 40, 50, 100, 150, 200, 250, 300, 400, 500, 600]
 
 spatial_method = ['kmeans', 'kmedoids-dijkstra']
 
@@ -101,7 +101,7 @@ for i in range (0, len(spatial_method)):
         
             idx = spatial_resolution[j]
 
-            etrago = Etrago(csv_folder_name="Calcs/"+col+"/"+str(idx))
+            etrago = Etrago(csv_folder_name="Dijkstra-Paper/Calcs/"+col+"/"+str(idx))
 
             network = etrago.network
             
@@ -141,19 +141,19 @@ for i in range (0, len(spatial_method)):
             
 # calculation time
 
-calc_time = pd.read_csv("Calcs/calc-time.csv")
+calc_time = pd.read_csv("Dijkstra-Paper/Calcs/calc-time.csv")
 calc_time.set_index("spatial resolution", inplace=True)
 
 '''# plots
 
 import matplotlib.pyplot as plt
 
-calc_time.plot(legend=True)
+calc_time.plot(legend=True, xlabel='number of nodes', ylabel='Calculation Time in Minutes')
 
 load_shedding.plot(legend=True)
 export.plot(legend=True)
 
-modularity.plot(legend=True)
+modularity.plot(legend=True, xlabel='number of nodes', ylabel='Modularity')
 
 system_costs.plot(legend=True)
 investment.plot(legend=True)
@@ -189,7 +189,7 @@ import networkx as nx
 
 import matplotlib.pyplot as plt
 
-spatial_resolution = [20, 50, 100, 150, 200, 250, 300, 400, 500]
+spatial_resolution = [20, 50, 100, 150, 200, 250, 300, 400, 500, 600]
 
 spatial_method = ['kmeans', 'kmedoids-dijkstra']
 
@@ -208,11 +208,13 @@ inv2_DE = inv1_DE.copy()
 # to plot some relative costs 
 # related to the result of the 500 nodes k-medoids Dijkstra clustered network
 
-etrago = Etrago(csv_folder_name="Calcs/kmedoids-dijkstra/500")
+etrago = Etrago(csv_folder_name="Dijkstra-Paper/Calcs/kmedoids-dijkstra/500")
 etrago.calc_results()
 
 relac_expansion =  pd.DataFrame(index=spatial_resolution, columns=spatial_method)
 base_relac = etrago.results.loc['annual ac grid investment costs'].value
+
+new_relac = pd.Series(index=spatial_resolution)
 
 reldc_expansion =  pd.DataFrame(index=spatial_resolution, columns=spatial_method)
 base_reldc = etrago.results.loc['annual dc grid investment costs'].value
@@ -243,7 +245,7 @@ for i in range (0, len(spatial_method)):
         
             idx = spatial_resolution[j]
     
-            etrago = Etrago(csv_folder_name="Calcs/"+col+"/"+str(idx))
+            etrago = Etrago(csv_folder_name="Dijkstra-Paper/Calcs/"+col+"/"+str(idx))
             
             etrago.calc_results()
             
@@ -267,14 +269,66 @@ for i in range (0, len(spatial_method)):
             relsto_expansion.at[idx, col] = etrago.results.loc['annual storage+store investment costs'].value / base_relsto
             rel_marginal.at[idx, col] = etrago.results.loc['annual marginal costs'].value / base_relmar
             rel_objective.at[idx, col] = etrago.results.loc['annual system costs'].value / base_objective
-
+            
+            if col=='kmedoids-dijkstra':
+                import pdb; pdb.set_trace()
+                base = Etrago(csv_folder_name="Dijkstra-Paper/Calcs/kmeans/"+str(idx))
+                base.calc_results()
+                base = base.results.loc['annual ac grid investment costs'].value
+                new_relac.loc[idx] = etrago.results.loc['annual ac grid investment costs'].value / base
+                
+# new_relac.plot()
 #relac_expansion.plot(legend=True)
 #reldc_expansion.plot(legend=True)
 #relsto_expansion.plot(legend=True)
 #rel_marginal.plot(legend=True)
 #rel_objective.plot(legend=True)
 
-# plot system costs as bar diagram
+# plot system costs as bar diagram - NEW
+
+costs1 = costs1 / 1000000000
+costs2 = costs2 / 1000000000
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+index11 = spatial_resolution.copy()
+index12 = spatial_resolution.copy()
+index13 = spatial_resolution.copy()
+index21 = spatial_resolution.copy()
+index22 = spatial_resolution.copy()
+index23 = spatial_resolution.copy()
+
+for i in range(0, len(index11)):
+    index11[i] = index11[i] - 8
+    index12[i] = index12[i] - 5
+    index13[i] = index13[i] - 2
+    index21[i] = index21[i] + 2
+    index22[i] = index22[i] + 5
+    index23[i] = index23[i] + 8
+    
+color1 = ['darkslategrey', 'darkcyan', 'skyblue']#, 'skyblue'] # kmeans
+color2 = ['maroon', 'tomato', 'lightsalmon']#, 'lightsalmon'] # kmedoids-djkstra
+
+col = costs1.columns
+
+plt.bar(index11, costs1[col[0]].values, width = 3, color=color1[0], label='k-means Clustring: marginal costs')#label='marginal costs')#,
+plt.bar(index12, costs1[col[1]].values, width = 3, color=color1[1], label='k-means Clustring: store investment')#label='store investment costs')#,
+plt.bar(index13, costs1[col[2]].values, width = 3, color=color1[2], label='k-means Clustring: electrical grid investment')#label='electrcial grid investment costs')#,
+
+plt.bar(index21, costs2[col[0]].values, width = 3, color=color2[0], label='k-medoids Dijkstra Clustering: marginal costs')
+plt.bar(index22, costs2[col[1]].values, width = 3, color=color2[1], label='k-medoids Dijkstra Clustering: store investment')
+plt.bar(index23, costs2[col[2]].values, width = 3, color=color2[2], label='k-medoids Dijkstra Clustering: electrical grid investment')
+
+plt.xticks(spatial_resolution)
+
+plt.legend(loc='upper right')
+plt.ylim([0,20])
+plt.ylabel('costs in billion Euro')
+plt.xlabel('number of nodes')
+#plt.title('System Costs depending on Spatial Resolution')
+ 
+# plot system costs as bar diagram - OLD
 
 costs1 = costs1 / 1000000000
 costs2 = costs2 / 1000000000
@@ -319,7 +373,7 @@ plt.ylabel('costs in billion Euro')
 plt.xlabel('number of nodes')
 plt.title('System Costs depending on Spatial Resolution')
 
-# plot investment costs as bar diagram
+# plot investment costs as bar diagram - OLD
 
 inv1 = inv1 / 1000000000
 inv2 = inv2 / 1000000000
@@ -356,7 +410,7 @@ plt.ylabel('costs in billion Euro')
 plt.xlabel('number of nodes')
 plt.title('Investment Costs depending on Spatial Resolution')
     
-# plot investment costs in Germany
+# plot investment costs in Germany - OLD
 
 inv1_DE = inv1_DE / 1000000000
 inv2_DE = inv2_DE / 1000000000
@@ -392,6 +446,10 @@ plt.legend(loc='lower right')
 plt.ylabel('costs in billion Euro')
 plt.xlabel('number of nodes')
 plt.title('Investment Costs in Germany depending on Spatial Resolution')
+
+###############################################################################
+
+# sectorcoupled model
     
 def compare_sector_coupled(path_dijkstra, path_kmeans):
     from matplotlib import pyplot as plt
@@ -408,7 +466,7 @@ def compare_sector_coupled(path_dijkstra, path_kmeans):
         ax.bar(df.index, df["diff[%]"])
         plt.ylabel("kmeans - dijkstra [%]")
 
-        filename = f"dijkstra-paper/{file}"
+        filename = f"{file}"
         pylab.savefig(filename, dpi=300, bbox_inches="tight")
 
         return
@@ -444,13 +502,27 @@ def compare_sector_coupled(path_dijkstra, path_kmeans):
     plt.title("Absolute diff in investment costs")
     ax.pie(inv_cost["diff"].abs(), labels=inv_cost.index)
 
-    filename = "dijkstra-paper/Absolute diff in investment costs"
+    filename = "Absolute diff in investment costs"
     pylab.savefig(filename, dpi=300, bbox_inches="tight")
 
-compare_sector_coupled("/home/student/git/eTraGo/etrago/dijkstra-paper/calculations/eGon2035",
-                       "/home/student/git/eTraGo/etrago/dijkstra-paper/calculations/eGon2035_kmeans")
-    
-def calculate_cross_transmission_cap(etrago_path, ddr_shape_path, save_path= False):
+compare_sector_coupled("etrago-results_15022023/eGon2035",
+                       "etrago-results_15022023/eGon2035_kmeans")
+
+###############################################################################
+
+# DDR vs BRD capacities
+
+import json
+import pypsa
+from etrago import Etrago
+
+import pandas as pd
+import networkx as nx
+
+import matplotlib.pyplot as plt
+
+  
+def calculate_cross_transmission_cap(etrago_path, ddr_shape_path, opt=True, save_path= False):
     from etrago import Etrago
     import geopandas as gpd
     from shapely.geometry import Point, LineString
@@ -481,14 +553,54 @@ def calculate_cross_transmission_cap(etrago_path, ddr_shape_path, save_path= Fal
     lines["bus1_in_ddr"] = lines["bus1"].map(buses["in_ddr"])
     lines["to_ddr"] = lines["bus0_in_ddr"] ^ lines["bus1_in_ddr"]
 
-    lines.plot()
+    #lines.plot()
     if save_path != False:
         lines.to_file(save_path, driver='GeoJSON')
     
-    return lines[lines["to_ddr"] == True]["s_nom"].sum()
+    if opt:
+        return lines[lines["to_ddr"] == True]["s_nom_opt"].sum()
+    else:
+        return lines[lines["to_ddr"] == True]["s_nom_min"].sum()
+    
+                                 
+spatial_resolution = [20, 50, 100, 150, 200, 250, 300, 400, 500, 600]
 
-calculate_cross_transmission_cap("/home/student/git/eTraGo/etrago/dijkstra-paper/calculations/eGon2035",
-                                 '/home/student/git/eTraGo/etrago/dijkstra-paper/gis/DDR.geojson')
+spatial_method = ['kmeans', 'kmedoids-dijkstra']
+
+capacities =  pd.DataFrame(index=spatial_resolution, columns=['k-means clustered', 'k-means optimized', 'k-medoids Dijkstra clustered', 'k-medoids Dijkstra optimized'])
+
+capacities.at[:, 'original'] = 19200#calculate_cross_transmission_cap("Dijkstra-Paper/orig",
+                                 #'DDR.geojson')
+
+for i in range (0, len(spatial_method)):
+    
+    col = spatial_method[i]
+    
+    if spatial_method[i] == 'kmeans':
+    
+        cols = ['k-means clustered', 'k-means optimized']
+        
+    else:
+        
+        cols = ['k-medoids Dijkstra clustered', 'k-medoids Dijkstra optimized']
+
+    for j in range(0, len(spatial_resolution)):
+        
+        idx = spatial_resolution[j]
+
+        clus = calculate_cross_transmission_cap("Dijkstra-Paper/Calcs/"+col+"/"+str(idx),
+                                 'DDR.geojson', opt=False)
+        
+        opt = calculate_cross_transmission_cap("Dijkstra-Paper/Calcs/"+col+"/"+str(idx),
+                                 'DDR.geojson')
+        
+        capacities.at[idx, cols[0]] = clus 
+        
+        capacities.at[idx, cols[1]] = opt
+        
+capacities.plot(legend=True, xlabel='number of nodes', ylabel='Former Border Capacity in MW', )
+
+capacities.drop(['k-means optimized', 'k-medoids Dijkstra optimized'], axis=1).plot(legend=True, xlabel='number of nodes', ylabel='Former Border Capacity in MW')
 
 
             
