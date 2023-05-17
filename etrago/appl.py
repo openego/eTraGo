012d51cfea8c377617b927329e1b353bff96da98
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2023-05-17 11:28:16 +0200

    changed end_snapshot

--- etrago/appl.py
+++ etrago/appl.py
@@ -63,7 +63,7 @@ args = {
         "q_allocation": "p_nom",  # allocate reactive power via 'p_nom' or 'p'
     },
     "start_snapshot": 1,
-    "end_snapshot": 2,
+    "end_snapshot": 10,
     "solver": "gurobi",  # glpk, cplex or gurobi
     "solver_options": {
         "BarConvTol": 1.0e-5,
