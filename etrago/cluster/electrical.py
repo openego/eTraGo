195bb88c73d600315e07b35d3dd6ba2d4453a269
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-10-19 15:27:59 +0200

    Add r and v_nom values from transformers to new lines

--- etrago/cluster/electrical.py
+++ etrago/cluster/electrical.py
@@ -720,6 +720,7 @@ def preprocessing(etrago):
             [
                 "bus0",
                 "bus1",
+                "r",
                 "x",
                 "s_nom",
                 "capital_cost",
@@ -732,6 +733,7 @@ def preprocessing(etrago):
             x=network.transformers.x
             * (380.0 / transformer_voltages.max(axis=1)) ** 2,
             length=1,
+            v_nom = 380.,
         )
         .set_index("T" + trafo_index),
         "Line",
