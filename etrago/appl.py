0d73294cb3d5dc676cf6016d9ee2349ebc384449
ulfmueller <ulf.p.mueller@hs-flensburg.de>
2024-01-15 14:51:37 +0100

    Update appl.py

--- etrago/appl.py
+++ etrago/appl.py
@@ -49,7 +49,7 @@ if "READTHEDOCS" not in os.environ:
 
 args = {
     # Setup and Configuration:
-    "db": "local2035",  # database session
+    "db": "egon-data",  # database session
     "gridversion": None,  # None for model_draft or Version number
     "method": {  # Choose method and settings for optimization
         "type": "market_grid",  # type of optimization, currently only 'lopf'
