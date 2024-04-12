d49dccb0ffbfda3cc4bb3e442296441bb3e0f152
ClaraBuettner <clara.buettner@hs-flensburg.de>
2024-04-12 19:57:04 +0200

    Fix dropping s_max_pu timeseries if sclopf is performed

--- etrago/appl.py
+++ etrago/appl.py
@@ -689,6 +689,13 @@ def run_etrago(args, json_path):
             etrago.network.generators.p_nom==0].index
         )
 
+    # Temporary drop DLR as it is currently not working with sclopf
+    if (etrago.args["method"]["type"] == "sclopf") & (
+            not etrago.network.lines_t.s_max_pu.empty):
+        print("Setting s_max_pu timeseries to 1")
+        etrago.network.lines_t.s_max_pu = pd.DataFrame(
+            index=etrago.network.snapshots,
+        )
 
     # adjust network regarding eTraGo setting
     etrago.adjust_network()
