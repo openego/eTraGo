6a65fe83851cf04c9b5fad896d21a9d5bcd01f35
ClaraBuettner <clara.buettner@hs-flensburg.de>
2024-08-13 12:28:07 +0200

    Apply black

--- noxfile.py
+++ noxfile.py
@@ -68,7 +68,9 @@ def build(session):
     current_version = f"{sys.version_info.major}.{sys.version_info.minor}"
     # Skip the session if it doesn't match the current CI Python version
     if session.python and session.python != current_version:
-        session.skip(f"Skipping tests for Python {session.python} since the current Python version is {current_version}.")
+        session.skip(
+            f"Skipping tests for Python {session.python} since the current Python version is {current_version}."
+        )
     setdefaults(session)
     session.install("twine")
     session.run("python", "setup.py", "bdist", "bdist_wheel")
@@ -81,7 +83,9 @@ def install(session):
     current_version = f"{sys.version_info.major}.{sys.version_info.minor}"
     # Skip the session if it doesn't match the current CI Python version
     if session.python and session.python != current_version:
-        session.skip(f"Skipping tests for Python {session.python} since the current Python version is {current_version}.")
+        session.skip(
+            f"Skipping tests for Python {session.python} since the current Python version is {current_version}."
+        )
     setdefaults(session)
     session.env["SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL"] = "False"
     session.run("python", "-mpip", "install", "--upgrade", "pip")
