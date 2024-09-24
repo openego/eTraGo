bad10bd9ae7fdd4693e6496fd5efc1530e490b55
Robert John <robert.john@ise.fraunhofer.de>
2024-09-24 13:46:49 +0200

    create results dct pre component and grid

--- etrago/validation/grid_components_comparison.py
+++ etrago/validation/grid_components_comparison.py
@@ -1,9 +1,14 @@
 import os
 import logging
-import pandas as pd
 from etrago import Etrago
 
+
 logger = logging.getLogger(__name__)
+
+
+def get_some_results(df):
+    # todo: compare something
+    return df
 
 
 def main(dir=None, export_p=None):
@@ -14,15 +19,27 @@ def main(dir=None, export_p=None):
         return
 
     # init results df
-    df = pd.DataFrame()
+    res_components_dct = {
+        "buses": {}, "lines": {}, "links": {}, "generators": {},
+        "loads": {}, "storage_units": {}, "transformers": {}
+    }
 
     for grid_name in os.listdir(dir):
         import_path = os.path.join(dir, grid_name)
         etrago = Etrago(csv_folder_name=import_path)
+        n = etrago.network
+
+        for component in res_components_dct.keys():
+            df = getattr(n, component)
+            df = get_some_results(df)
+            res_components_dct[component][grid_name] = df
 
     if export_p is not None:
+        msg = ("TODO: do something simple with res_components_dct, "
+               "e.g. v_nom.value_counts, line type, -length, s_nom, ...")
+        print(msg)
         # export if export path is provided
-        df.to_csv(export_p)
+        # df.to_csv(export_p)
 
 
 if __name__ == "__main__":
