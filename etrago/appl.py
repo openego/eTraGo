a1ad437b8279b3c6be3e4c437dbae1ec023af302
ClaraBuettner <clara.buettner@hs-flensburg.de>
2024-11-07 09:44:58 +0100

    Drop generators without installed capacity

--- etrago/appl.py
+++ etrago/appl.py
@@ -683,6 +683,8 @@ def run_etrago(args, json_path):
 
     # import network from database
     etrago.build_network_from_db()
+    etrago.network.mremove("Generator", etrago.network.generators[
+        etrago.network.generators.p_nom==0].index)
 
     # adjust network regarding eTraGo setting
     etrago.adjust_network()
