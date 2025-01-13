9f2239df73c1cc0efaa1404f07ef7954ff2b84a1
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-01-13 13:34:07 +0100

    Set scenario name to eGon100RE

--- etrago/appl.py
+++ etrago/appl.py
@@ -83,7 +83,7 @@ args = {
         "threads": 4,
     },
     "model_formulation": "kirchhoff",  # angles or kirchhoff
-    "scn_name": "eGon2035",  # scenario: eGon2035, eGon100RE or status2019
+    "scn_name": "eGon100RE",  # scenario: eGon2035, eGon100RE or status2019
     # Scenario variations:
     "scn_extension": None,  # None or array of extension scenarios
     "scn_decommissioning": None,  # None or decommissioning scenario
