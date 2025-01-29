ff33849ec62d452e954aac1d2f70c45a00ad7929
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-01-29 16:52:30 +0100

    Deals with #776: Combine low_voltage buses in foreign countries with AC

--- etrago/tools/utilities.py
+++ etrago/tools/utilities.py
@@ -3112,6 +3112,25 @@ def manual_fixes_datamodel(etrago):
                 etrago.network.buses[etrago.network.buses.country!="DE"].index))].copy()
         etrago.network.stores.loc[ext_foreign_stores.index, "e_nom_extendable"] = True
 
+        # Temporary drop low_voltage buses in foreign countries and combine
+        # them with AC
+        low_voltage_buses = etrago.network.buses.loc[
+            etrago.network.buses.carrier == "low_voltage"].copy()
+        for i, row in low_voltage_buses.iterrows():
+            link = etrago.network.links[
+                (etrago.network.links.bus1==i)&
+                (etrago.network.links.carrier=="electricity_distribution_grid")
+                ]
+            new_bus = link.bus0.unique()[0]
+            for comp in etrago.network.iterate_components():
+                if "bus" in comp.df.columns:
+                    comp.df.loc[comp.df.bus==i, "bus"] = new_bus
+                elif "bus0" in comp.df.columns:
+                    comp.df.loc[comp.df.bus0==i, "bus0"] = new_bus
+                    comp.df.loc[comp.df.bus1==i, "bus1"] = new_bus
+            etrago.network.remove("Bus", i)
+            etrago.network.mremove("Link", link.index)
+
     # Add static p-set to other AC load in foreign countries
     static_ac_loads = etrago.network.loads[
         (etrago.network.loads.carrier=="AC") &
