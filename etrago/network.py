0e99ff10e11e3f394dc49ff4bfc94a943ae93f3c
ClaraBuettner <clara.buettner@hs-flensburg.de>
2026-01-30 08:47:35 +0100

    Integrate seperation of distribution grids

--- etrago/network.py
+++ etrago/network.py
@@ -81,6 +81,7 @@ from etrago.execute.sclopf import (
     iterate_sclopf,
     post_contingency_analysis_lopf,
 )
+from etrago.tools.distribution_grids import add_simplified_distribution_grids
 from etrago.tools.extendable import extendable
 from etrago.tools.io import (
     NetworkScenario,
@@ -427,6 +428,8 @@ class Etrago:
 
     levelize_abroad_inland_parameters = levelize_abroad_inland_parameters
 
+    add_simplified_distribution_grids = add_simplified_distribution_grids
+
     def dc_lines(self):
         return self.filter_links_by_carrier("DC", like=False)
 
@@ -459,6 +462,10 @@ class Etrago:
             self.add_ch4_h2_correspondence()
 
         logger.info("Imported network from db")
+
+        if self.args["method"]["distribution_grids"]:
+            self.add_simplified_distribution_grids()
+            logger.info("Added simplified distribution grids")
 
     def adjust_network(self):
         """
