fa2862766fbf1ce97a6ce6cf838adf1096a64183
KathiEsterl <katharina.esterl@uni-flensburg.de>
2026-05-05 10:25:20 +0200

    Set control strategy neg load shedding

--- etrago/tools/utilities.py
+++ etrago/tools/utilities.py
@@ -764,6 +764,7 @@ def set_control_strategies(network):
         network.generators.carrier.isin(
             [
                 "load shedding",
+                "negative load shedding",
                 "CH4",
                 "CH4_biogas",
                 "CH4_NG",
