fe631bf3836ad2208e033e6b98e36b2975dcacf3
KathiEsterl <katharina.esterl@uni-flensburg.de>
2026-07-22 15:50:01 +0200

    Add export of buses within focus region, info is needed within ego workflow later

--- etrago/cluster/spatial.py
+++ etrago/cluster/spatial.py
@@ -933,6 +933,8 @@ def focus_weighting(
     # Dijkstra's algorithm will be used for path lengths of lines
     # considering buses at border of focus region to consider its size
     if "AC" in network.buses.carrier.unique():
+        os.makedirs(etrago.args["csv_export"]+'/clustering', exist_ok=True)
+        inside.to_csv(etrago.args["csv_export"]+'/clustering/buses_within_focus.csv')
         lines_cross = network.lines[
             (network.lines.bus0.isin(inside.index))
             ^ (network.lines.bus1.isin(inside.index))
