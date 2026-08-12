0592f0c21f73ba83e84973fa9e1d1f52a0388118
ClaraBuettner <clara.buettner@hs-flensburg.de>
2026-08-12 11:34:21 +0200

    Fix setting scn_name for new components

--- etrago/tools/distribution_grids.py
+++ etrago/tools/distribution_grids.py
@@ -756,7 +756,7 @@ def seperate_demands(
         names=hv_ind_loads_p.columns.astype(str) + "_transmission_grid",
         bus=hv_ind_loads_p.columns.values,
         carrier="AC",
-        scn_name="eGon2035",
+        scn_name=self.args["scn_name"],
     )
 
     self.network.loads_t.p_set.loc[
