9001da9c04a757f71f293ed92526f574ac53629a
Katharina Esterl <47177587+KathiEsterl@users.noreply.github.com>
2026-08-25 15:59:46 +0200

    Remove export of focus region buses

--- etrago/cluster/spatial.py
+++ etrago/cluster/spatial.py
@@ -934,10 +934,6 @@ def focus_weighting(
     # Dijkstra's algorithm will be used for path lengths of lines
     # considering buses at border of focus region to consider its size
     if "AC" in network.buses.carrier.unique():
-        os.makedirs(etrago.args["export_results_path"], exist_ok=True)
-        inside.to_csv(
-            etrago.args["export_results_path"] + "/buses_within_focus.csv"
-        )
         lines_cross = network.lines[
             (network.lines.bus0.isin(inside.index))
             ^ (network.lines.bus1.isin(inside.index))
